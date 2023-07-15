615216665ed65353e8e9b9cab484cc37a84878e5
Juan Pablo Sarobe <jp.sarobe@gmail.com>
2023-07-15 19:03:23 -0300

    orden etapa 2 terminada

--- clase2/clase2_tp.js
+++ clase2/clase2_tp.js
@@ -1,2 +1,10 @@
 // Ejercicio de practica de la clase 2
 
+class Usuario {
+  constructor(nombre, apellido, libros, mascotas) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros;
+    this.mascotas = mascotas;
+  }
+}
